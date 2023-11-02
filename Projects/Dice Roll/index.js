3a8ef054681b2b5cee4fe3709de8d1fb5e95efbc
SeanChen <xiaolaiseanchen@gmail.com>
2023-11-02 15:51:33 -0700

    Add more projects

---
+++ Projects/Dice Roll/index.js
@@ -0,0 +1,8 @@
+
+function game(){
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+    var randomDiceImageSrc1 = `./images/dice${randomNumber1}.png`
+    var randomDiceImageSrc1 = `./images/dice${randomNumber1}.png`
+}
